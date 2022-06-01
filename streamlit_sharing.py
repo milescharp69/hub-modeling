2e6d5341b19bb1355b82aaedc77e2b030778f4ce
Miles <miles.charpentercca@gmail.com>
2022-06-01 12:11:43 -0500

    Added data to the bargraph

--- streamlit_sharing.py
+++ streamlit_sharing.py
@@ -48,6 +48,9 @@ Hub_Vehicle_Mix = [0.1, 0.35 , 0.55]
 Vehicle_Classes = [Class_A,Class_B,Class_C]
 Hub_Commercial_Dominant = Hub(Hub_Name, Hub_Notional_Loading, Hub_Ports, Hub_Vehicle_Mix, Vehicle_Classes)
 
+definedHubs = [Hub_Rural, Hub_Urban_Community, Hub_Urban_Multimodal, Hub_Commercial_Dominant]
+definedVehicleClasses = list(Vehicle_Classes)
+
 # #Hub Page
 st.set_page_config(layout="wide")
 st.title("Hub Modeling")
@@ -131,6 +134,37 @@ st_echarts(options=pieGraph, width="100%", key=0)
 
 
 #option={"tooltip":{"trigger":"axis","axisPointer":{"type":"shadow"}},"legend":{},"grid":{"left":"3%","right":"4%","bottom":"3%","containLabel":!0},"xAxis":[{"type":"category","data":["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]}],"yAxis":[{"type":"value"}],series:[{name:"Direct",type:"bar",emphasis:{focus:"series"},data:[320,332,301,334,390,330,320]},{name:"Email",type:"bar",stack:"Ad",emphasis:{focus:"series"},data:[120,132,101,134,90,230,210]},{name:"Union Ads",type:"bar",stack:"Ad",emphasis:{focus:"series"},data:[220,182,191,234,290,330,310]},{name:"Video Ads",type:"bar",stack:"Ad",emphasis:{focus:"series"},data:[150,232,201,154,190,330,410]},{name:"Search Engine",type:"bar",data:[862,1018,964,1026,1679,1600,1570],emphasis:{focus:"series"},markLine:{lineStyle:{type:"dashed"},data:[[{type:"min"},{type:"max"}]]}},{name:"Baidu",type:"bar",barWidth:5,stack:"Search Engine",emphasis:{focus:"series"},data:[620,732,701,734,1090,1130,1120]},{name:"Google",type:"bar",stack:"Search Engine",emphasis:{focus:"series"},data:[120,132,101,134,290,230,220]},{name:"Bing",type:"bar",stack:"Search Engine",emphasis:{focus:"series"},data:[60,72,71,74,190,130,110]},{name:"Others",type:"bar",stack:"Search Engine",emphasis:{focus:"series"},data:[62,82,91,84,109,110,120]}]}
+
+BARGRAPH_xaxislabels = [definedHubs[i].Hub_Type for i in range(len(definedHubs))]
+definedVehicleClasses
+BARGRAPH_seriesdata = [{
+    "name": "Class 1-2",
+    "type": 'bar',
+    "emphasis": {
+        "focus": 'series'
+    },
+    "data": [definedHubs[i].Vehicles_Serviced_Per_Month_By_Class(0) for i in range(len(definedHubs))]
+},
+{
+    "name": "Class 3-6",
+    "type": 'bar',
+    "emphasis": {
+        "focus": 'series'
+    },
+    "data": [definedHubs[i].Vehicles_Serviced_Per_Month_By_Class(1) for i in range(len(definedHubs))]
+},
+{
+    "name": "Class 7-8",
+    "type": 'bar',
+    "emphasis": {
+        "focus": 'series'
+    },
+    "data": [definedHubs[i].Vehicles_Serviced_Per_Month_By_Class(2) for i in range(len(definedHubs))]
+}
+]
+
+
+
 option = {
   "tooltip": {
     "trigger": 'axis',
@@ -148,7 +182,7 @@ option = {
   "xAxis": [
     {
       "type": 'category',
-      "data": ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      "data": BARGRAPH_xaxislabels
     }
   ],
   "yAxis": [
@@ -156,93 +190,6 @@ option = {
       "type": 'value'
     }
   ],
-  "series": [
-    {
-      "name": 'Direct',
-      "type": 'bar',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [320, 332, 301, 334, 390, 330, 320]
-    },
-    {
-      "name": 'Email',
-      "type": 'bar',
-      "stack": 'Ad',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [120, 132, 101, 134, 90, 230, 210]
-    },
-    {
-      "name": 'Union Ads',
-      "type": 'bar',
-      "stack": 'Ad',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [220, 182, 191, 234, 290, 330, 310]
-    },
-    {
-      "name": 'Video Ads',
-      "type": 'bar',
-      "stack": 'Ad',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [150, 232, 201, 154, 190, 330, 410]
-    },
-    {
-      "name": 'Search Engine',
-      "type": 'bar',
-      "data": [862, 1018, 964, 1026, 1679, 1600, 1570],
-      "emphasis": {
-        "focus": 'series'
-      },
-      "markLine": {
-        "lineStyle": {
-          "type": 'dashed'
-        },
-        "data": [[{ "type": 'min' }, { "type": 'max' }]]
-      }
-    },
-    {
-      "name": 'Baidu',
-      "type": 'bar',
-      "barWidth": 5,
-      "stack": 'Search Engine',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [620, 732, 701, 734, 1090, 1130, 1120]
-    },
-    {
-      "name": 'Google',
-      "type": 'bar',
-      "stack": 'Search Engine',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [120, 132, 101, 134, 290, 230, 220]
-    },
-    {
-      "name": 'Bing',
-      "type": 'bar',
-      "stack": 'Search Engine',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [60, 72, 71, 74, 190, 130, 110]
-    },
-    {
-      "name": 'Others',
-      "type": 'bar',
-      "stack": 'Search Engine',
-      "emphasis": {
-        "focus": 'series'
-      },
-      "data": [62, 82, 91, 84, 109, 110, 120]
-    }
-  ]
+  "series": BARGRAPH_seriesdata
 }
 st_echarts(options=option, width="100%", key=1)
